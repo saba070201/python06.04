d39864a3e99fa3f97c7800babcaf0a30745f6378
saba070201 <saba07022001@yandex.ru>
2023-04-27 20:57:25 +0300

    read

--- lesson6/main.py
+++ lesson6/main.py
@@ -1,10 +1,13 @@
 import numpy as np
 import random
+import time
 PATH_100='lesson6/arrays/100.txt'
 PATH_1000='lesson6/arrays/1000.txt'
 PATH_10000='lesson6/arrays/10000.txt'
 PATH_100000='lesson6/arrays/100000.txt'
 PATH_1000000='lesson6/arrays/1000000.txt'
+PATH_RESULT='lesson6/arrays/result.txt'
+
 def write_arr_to_file(path,size):
     a=[random.randint(-1000,1000) for i in range(size)]
     with open(path,'w') as f:
@@ -16,4 +19,18 @@ def read_array_from_file(path):
         for i in f:
             arr.append(int(i))
     return arr
+def writeresult(path,data):
+    with open(path,'a+') as f:
+        f.write(str(data)+'\n')
+def bubblesort(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
 
+temparr=read_array_from_file(PATH_100000)   
+stime=time.time()
+bubblesort(temparr)
+ftime=time.time()
+fftime=ftime-stime
+writeresult(PATH_RESULT,f'bubblesort100000 {fftime}')
