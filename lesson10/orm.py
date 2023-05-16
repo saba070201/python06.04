faae8186ad5d6ddb08aa6548165f767372679b0e
saba070201 <saba07022001@yandex.ru>
2023-05-16 21:50:29 +0300

    l10f

---
+++ lesson10/orm.py
@@ -0,0 +1,12 @@
+from peewee import *
+conn=SqliteDatabase('test.db')
+
+class User(Model):
+    id=PrimaryKeyField(column_name='id')
+    name=TextField(column_name='name')
+    class Meta:
+        table_name='users'
+        database=conn
+
+for i in User.select().where(User.name=='misha1'):
+    print(i.name)
