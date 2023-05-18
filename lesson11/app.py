57a3ac0ec813b2a27b764e0ad5b10bcc5cd2a9fd
saba070201 <saba07022001@yandex.ru>
2023-05-18 20:58:56 +0300

    l1sf

---
+++ lesson11/app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+from models import * 
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+db = SQLAlchemy()
+db.init_app(app)
+
+@app.route('/')
+def home():
+    data=db.session.execute(db.select(Notes)).scalars()
+
+    return render_template('index.html',data=data)
+
+
+@app.route('/note-<int:note_id>')
+def note(note_id):
+    note_data=db.get_or_404(Notes,note_id)
+    return render_template('note.html',note_data=note_data)
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
---
+++ lesson11/models.py
@@ -0,0 +1,9 @@
+from app import db ,app 
+class Notes(db.Model):
+    __tablename__='notes'
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(250),nullable=False)
+    memo=db.Column(db.String(5000),nullable=False)
+    important=db.Column(db.Boolean,nullable=False)
+with app.app_context():
+    db.create_all()
