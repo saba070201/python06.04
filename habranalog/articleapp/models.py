29c35c5b757defefdfacb8d4618676a1c8c07b51
saba070201 <saba07022001@yandex.ru>
2023-05-25 19:37:51 +0300

    l13s

---
+++ habranalog/articleapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Article(models.Model):
+    title=models.CharField(max_length=150)
+    prememo=models.TextField(null=True,blank=True)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    date_published=models.DateField(auto_now=True)
+# Create your models here.
